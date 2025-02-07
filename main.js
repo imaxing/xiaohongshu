8b2ebf6135b37b86dfb87a2fe06d37867a369932
oreo <oreo@cookie.com>
2025-02-07 16:53:10 +0800

    feat(puppeteer): 测试puppeteer模拟操作

--- main.js
+++ main.js
@@ -2,9 +2,12 @@
 const { app, BrowserWindow, ipcMain, dialog } = require("electron");
 const path = require("node:path");
 const _robot = require("@jitsi/robotjs");
+const puppeteer = require('puppeteer-extra');
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 const { RobotEventIpc } = require("./robot");
 const robot = new RobotEventIpc();
 robot.initRobtIpc();
+puppeteer.use(StealthPlugin());
 
 
 const results = {}
@@ -38,27 +41,60 @@ const events = {
   }
 };
 
-function createWindow() {
-  const mainWindow = new BrowserWindow({
-    width: 1500,
-    height: 1200,
-    webPreferences: {
-      sandbox: true,
-      nodeIntegration: false,
-      contextIsolation: true,
-      preload: path.join(__dirname, "preload.js"),
-    },
-  });
+// function createWindow() {
+//   const mainWindow = new BrowserWindow({
+//     width: 1500,
+//     height: 1200,
+//     webPreferences: {
+//       sandbox: true,
+//       nodeIntegration: false,
+//       contextIsolation: true,
+//       preload: path.join(__dirname, "preload.js"),
+//     },
+//   });
 
 
-  mainWindow.webContents.setUserAgent(
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36"
-  );
+//   mainWindow.webContents.setUserAgent(
+//     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36"
+//   );
 
-  mainWindow.loadURL(`https://www.xiaohongshu.com/explore`);
+//   mainWindow.loadURL(`https://www.xiaohongshu.com/explore`);
 
 
-  !app.isPackaged && mainWindow.webContents.openDevTools();
+//   !app.isPackaged && mainWindow.webContents.openDevTools();
+
+
+
+//   // 获取初始窗口位置
+//   const [x, y] = mainWindow.getPosition();
+//   const [width, height] = mainWindow.getSize();
+//   mainWindow.webContents.send('window-info', {x, y, width, height});
+
+//   // 监听窗口移动事件
+//   mainWindow.on('move', () => {
+//     const [x, y] = mainWindow.getPosition();
+//     const [width, height] = mainWindow.getSize();
+//     mainWindow.webContents.send('window-info', {x, y, width, height});
+//   });
+
+//   // 监听窗口大小变化事件
+//   mainWindow.on('resize', () => {
+//     const [x, y] = mainWindow.getPosition();
+//     const [width, height] = mainWindow.getSize();
+//     mainWindow.webContents.send('window-info', {x, y, width, height});
+//   });
+
+// }
+
+// app.whenReady().then(async () => {
+//   createWindow();
+//   app.on("activate", function () {
+//     if (BrowserWindow.getAllWindows().length === 0) createWindow();
+//   });
+// });
+
+
+
 
   ipcMain.on("event", (_, d) => {
     const { type, data } = JSON.parse(d);
@@ -68,36 +104,36 @@ function createWindow() {
   });
 
 
-  // 获取初始窗口位置
-  const [x, y] = mainWindow.getPosition();
-  const [width, height] = mainWindow.getSize();
-  mainWindow.webContents.send('window-info', {x, y, width, height});
+app.whenReady().then(async () => {
 
-  // 监听窗口移动事件
-  mainWindow.on('move', () => {
-    const [x, y] = mainWindow.getPosition();
-    const [width, height] = mainWindow.getSize();
-    mainWindow.webContents.send('window-info', {x, y, width, height});
-  });
+  // 启动浏览器
+  const browser = await puppeteer.launch({ 
+    headless: false,
+    slowMo: 100,
+   });
+  const page = await browser.newPage();
+  page.setUserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36")
 
-  // 监听窗口大小变化事件
-  mainWindow.on('resize', () => {
-    const [x, y] = mainWindow.getPosition();
-    const [width, height] = mainWindow.getSize();
-    mainWindow.webContents.send('window-info', {x, y, width, height});
-  });
+  // 导航到目标页面
+  await page.goto('https://xiaohongshu.com/explore');
 
-}
+  // `#exploreFeeds > section[data-index="1"] a.cover`
+  // 等待元素加载
+  await page.waitForSelector('#exploreFeeds > section[data-index="0"] a.cover');
 
-app.whenReady().then(() => {
-  createWindow();
-  app.on("activate", function () {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
-  });
-});
+  // 获取元素位置信息
+  const element = await page.$('#exploreFeeds > section[data-index="0"] a.cover');
+  const box = await element.boundingBox();
+
+  // 移动鼠标到元素中心点
+  await page.mouse.move(box.x + box.width/2, box.y + box.height/2);
+  
+  // 点击元素
+  await page.mouse.click(box.x + box.width/2, box.y + box.height/2);
+
+})
+
 
 app.on("window-all-closed", function () {
   if (process.platform !== "darwin") app.quit();
 });
-
-
